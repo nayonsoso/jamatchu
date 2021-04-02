53387fab53c4d21a58a62f0f5348dcb2af0891eb
nayonsoso <panda0329@naver.com>
2021-04-02 20:38:24 +0900

    RealFinal

--- food/src/setupProxy.js
+++ food/src/setupProxy.js
@@ -1,6 +1,6 @@
 const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    app.use("/#/test",
+    app.use("/#/",
         createProxyMiddleware({ target: "http://localhost:5000",changeOrigin: true, }));
 };
